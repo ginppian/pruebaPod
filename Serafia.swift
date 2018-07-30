b1aae80b9449c9783080b04391f0541205dfad55
Gmo Ginppian <davidguillermo.lopez.contractor@bbva.com>
2018-07-30 10:41:22 -0500

    Fist commit

---
+++ Serafia.swift
@@ -0,0 +1,16 @@
+//
+//  Serafia.swift
+//  cuarPod
+//
+//  Created by Gmo Ginppian on 30/07/18.
+//  Copyright © 2018 BUAP. All rights reserved.
+//
+
+import Foundation
+
+class Serafia: NSObject {
+    class func Soft() -> String {
+        return "Hola soy serafia soft"
+    }
+}
+
---
+++ cuarPod.h
@@ -0,0 +1,19 @@
+//
+//  cuarPod.h
+//  cuarPod
+//
+//  Created by Gmo Ginppian on 30/07/18.
+//  Copyright © 2018 BUAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cuarPod.
+FOUNDATION_EXPORT double cuarPodVersionNumber;
+
+//! Project version string for cuarPod.
+FOUNDATION_EXPORT const unsigned char cuarPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cuarPod/PublicHeader.h>
+
+
